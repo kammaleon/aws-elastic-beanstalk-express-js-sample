beb297e52e7a443fb59c54156bde3d65e7d9811f
kammaleon <miguela.alfonso.figueredo@gmail.com>
2022-03-13 19:06:47 +0100

    Full Pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('APRENDO ALGO NUEVO!!!'));
+app.get('/', (req, res) => res.send('APRENDO ALGO NUEVO!!! ....Full pipeline test '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
